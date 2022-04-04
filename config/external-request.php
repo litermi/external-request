e440aeed531e53ccea2ced7434ddf3cea0458279
cirel.ramos <cirel.ramos@litermi.com>
2022-04-04 11:33:45 -0500

    feat(src, config): add new config and adjust logic to get values from config part 2

---
+++ config/external-request.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel External Request
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+
+    /*
+     * default header to request
+     * example:
+     */
+    'default_parameters_to_header' => [
+        'cache-control' => 'no-cache',
+        'Content-Type' => 'application/json',
+    ],
+
+    /*
+     * get special values from header
+     * example:
+     */
+    'get_special_values_from_header' => [
+        'special' => 'value',
+    ],
+
+
+    /*
+     * get special values from request
+     * example:
+     */
+    'get_special_values_from_request' => [
+        'ip' => 'ip',
+    ],
+];
